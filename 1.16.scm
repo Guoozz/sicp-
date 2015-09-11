dd5c5d8cc3efd37dfb8024899e65448b1baccddd
Guoozz <csguozhen@gmail.com>
2015-09-11 19:04:10 +0800

    错误修正

--- 1.16.scm
+++ 1.16.scm
@@ -9,4 +9,4 @@
 (define (fast-expt-iter product b n)
   (cond ( (= n 0) product)
         ( (even? n) (fast-expt-iter product  (square b)  (/ n 2)))
-        ( else (fast-expt-iter (* product b) b (- n 1)))));; 需要注意的是n无论取值(0除外) 最后一步变换n总是等于1
+        ( else (fast-expt-iter (* product b) b (- n 1)))));;
