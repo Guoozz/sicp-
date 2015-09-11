16adc316c9ba8d04b5ab15f2c1c7098b2894dde1
Guoozz <csguozhen@gmail.com>
2015-09-11 16:41:15 +0800

    帕斯卡三角组合版本

--- 1.12.scm
+++ 1.12.scm
@@ -2,4 +2,19 @@
   (cond ((> col row ) (error "input error col > row"))
         ((or (= col 1) (= col row)) 1)
         (else (+ (Pascal-triangle (- row 1) (- col 1))
-            (Pascal-triangle (- row 1) col)))))
+                 (Pascal-triangle (- row 1) col)))))
+
+
+;; 迭代版本 利用组合公式求
+(define (factorial n)
+  (factorial-iter 1  n))
+
+(define (factorial-iter product n)
+  (if (= 1 n) product
+      (factorial-iter (* product n) (- n 1))))
+
+(define (c n m)
+  (/ (factorial n) (* (factorial m) (factorial (- n m)))))
+
+(define (Pascal-triangle2 row col)
+  (c (- row 1)  (- col 1)))
