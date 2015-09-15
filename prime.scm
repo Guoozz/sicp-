2689d0d982c311412feaa1679143362d60c578cf
Guoozz <csguozhen@gmail.com>
2015-09-15 23:03:38 +0800

    details

---
+++ prime.scm
@@ -0,0 +1,24 @@
+(define (expond a n)
+  (define (even? x) (= (remainder x 2) 0))
+  (define (square x) (* x x))
+  (if (= n 0)
+      1
+      (if (even? n)
+          (square (expond a (/ n 2)))
+          (* a (expond a (- n 1))))))
+
+(define (exp-mod base exp m)
+  (remainder (expond base exp) m))
+
+
+(define (ferma-test n)
+  (define (try-it a)
+    (= (exp-mod a n n) a))
+  (try-it (+ 1 (random (- n 1)))))
+
+(define (prime? n)
+  (define (fast-prime? n times)
+    (cond ((= times 0) true)
+          ((ferma-test n) (fast-prime? n (- times 1)))
+          (else false)))
+  (fast-prime? n 10))
