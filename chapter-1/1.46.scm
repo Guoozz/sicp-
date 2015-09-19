7f3fa4482bb8d7c5e509e268b794009f9687d706
Guoozz <csguozhen@gmail.com>
2015-09-19 14:02:29 +0800

    第一章习题结束

---
+++ chapter-1/1.46.scm
@@ -0,0 +1,17 @@
+(load "fixed-point.scm")
+
+(define (iterative-improve enough? improve)
+  (lambda (x)
+    (let ((next (improve x)))
+      (if (enough? x next)
+          x
+          ((iterative-improve enough? improve) next)))))
+
+(define (close-enough? guess next)
+    (< (abs (- guess next)) 0.00001))
+
+(define (fixed-point f guess)
+  ((iterative-improve close-enough? f) guess))
+
+(define (sqrt x)
+  (fixed-point (lambda (y) (average y (/ x y))) 1.0))
