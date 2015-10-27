c0d10c098a9767e560b27b4bef22bff08f5c035c
Guoozz <csguozhen@gmail.com>
2015-10-27 09:27:57 +0800

    细节修改"

---
+++ chapter-2/README.md
@@ -0,0 +1,14 @@
+### 关于习题**2.86**
+
+-------------------------------------------------------------------------------
+这个问题解决的办法不是很困难,除了内置型(scheme-number)数据外,其他数据类型的运算都要采用通用型计算过程,例如 x,y 为复数那么
+
+``` scheme
+(define (add-complex x y)
+ (make-from-real-imag (add (real-part x)
+                           (real-part y))
+                      (add (imag-part x)
+                           (imag-part y)
+```
+
+其中add为通用计算过程
--- chapter-2/polar-complex-package.scm
+++ chapter-2/polar-complex-package.scm
@@ -1,11 +1,11 @@
 (define (install-polar-package)
   (define (magnitude z) (car z))
   (define (angle z) (cdr z))
-  (define (real-part z) (* (magnitude z) (cos (angle z))))
-  (define (imag-part z) (* (magnitude z) (sin (angle z))))
+  (define (real-part z) (mul (magnitude z) (cos (angle z))))
+  (define (imag-part z) (mul (magnitude z) (sin (angle z))))
   (define (make-from-mag-ang r a) (cons r a))
   (define (make-from-real-imag x y)
-    (cons (sqrt (+ (square x) (square y)))
+    (cons (sqrt (add (square x) (square y)))
           (atan y x)))
 
   ;; interface to the rest of system
