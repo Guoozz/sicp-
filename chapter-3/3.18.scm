fbcb3b1fd8ba1b7faed57124adcfa21061af6585
Guoozz <csguozhen@gmail.com>
2015-11-19 19:36:10 +0800

    touch

---
+++ chapter-3/3.18.scm
@@ -0,0 +1,16 @@
+;; 判断一个表是否包含环
+
+(define (circle-list? l)
+  (define (iter rest visited-set)
+    (cond ((null? rest) false)
+          ((in-set? (cdr rest) visited-set) true)
+          (else (iter (cdr rest) (adjoin (cdr rest) visited-set)))))
+  (iter l '()))
+
+(define (in-set? x l)
+  (cond ((null? l) false)
+        ((eq? (car l) x) true)
+        (else (in-set? x (cdr l)))))
+
+(define (adjoin item set)
+  (cons item set))
